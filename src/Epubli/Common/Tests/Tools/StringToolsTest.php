04953b566fe1b07fd187be9cf7b137766d19d698
Simon Schrape <s.schrape@epubli.com>
2015-09-22 16:48:39 +0200

    Copy functions from epubli-symfony StringUtils.

--- src/Epubli/Common/Tests/Tools/StringToolsTest.php
+++ src/Epubli/Common/Tests/Tools/StringToolsTest.php
@@ -7,6 +7,29 @@ use PHPUnit_Framework_TestCase;
 
 class StringToolsTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * @param string $search The substring to be replaced.
+     * @param string $replace The replacement string.
+     * @param string $subject The original string.
+     * @param string $expected The expected result.
+     * @dataProvider provideReplaceFirstOccurrenceData
+     */
+    public function testReplaceFirstOccurrence($search, $replace, $subject, $expected)
+    {
+        $result = StringTools::replaceFirstOccurrence($search, $replace, $subject);
+        $this->assertEquals($expected, $result);
+    }
+
+    public function provideReplaceFirstOccurrenceData()
+    {
+        return [
+            ['needle', 'camel', 'Haystack containing a needle.', 'Haystack containing a camel.'],
+            ['needle', 'camel', 'Needlestack containing a needle.', 'Needlestack containing a camel.'],
+            ['Needle', 'Camel', 'Needlestack containing a needle.', 'Camelstack containing a needle.'],
+            ['/\\$.*?()[]^', '\\1$1', '/\\$.*?()[]^/\\$.*?()[]^', '\\1$1/\\$.*?()[]^'],
+        ];
+    }
+
     public function testStripNonAsciiChars()
     {
         $this->assertEquals(
--- src/Epubli/Common/Tools/StringTools.php
+++ src/Epubli/Common/Tools/StringTools.php
@@ -5,6 +5,19 @@ namespace Epubli\Common\Tools;
 class StringTools
 {
     /**
+     * Replace only first occurrence of a substring.
+     * @param string $search The substring to be replaced.
+     * @param string $replace The replacement string.
+     * @param string $subject The original string.
+     * @return string The resulting string.
+     */
+    public static function replaceFirstOccurrence($search, $replace, $subject)
+    {
+        $pos = strpos($subject, $search);
+        return $pos === false ? $subject : substr_replace($subject, $replace, $pos, strlen($search));
+    }
+
+    /**
      * Strip non-ascii characters
      * @see http://stackoverflow.com/questions/8781911/remove-non-ascii-characters-from-string-in-php#answer-8781968
 
