563f34b96b22fcd41a82ed9572103e4659b9228c
Timor Kodal <timor@kodal.de>
2017-04-20 13:59:48 +0200

    WD-60: introduced two new methods for human readable value representations (i.e. bytes, seconds)

--- src/Epubli/Common/Tools/StringTools.php
+++ src/Epubli/Common/Tools/StringTools.php
@@ -18,6 +18,50 @@ class StringTools
     }
 
     /**
+     * Returns a human readable representation of elapsed time durations
+     * @param $value
+     * @param int $digits
+     * @param bool $fullName
+     * @return string
+     */
+    public static function humanReadableSeconds($value, $digits = 2, $fullName = false) {
+        $dimensions = [
+            "factor"=>[60,60,60,24,7,4],
+            "short"=>["s","m","h","d","w","m"],
+            "long"=>["Seconds","Minutes","Hours","Days","Weeks","Months"]
+        ];
+        $dim = 0;
+        $buf = $value;
+        while($buf > $factor = $dimensions["factor"][$dim]) {
+            $dim ++;
+            $buf = $buf / $factor;
+        }
+        return sprintf("%02.{$digits}f", $buf)." ".$dimensions[($fullName ? 'long':'short')][$dim];
+    }
+
+    /**
+     * Returns a human readable representation of bytes (filesizes, memory etc)
+     * @param $value
+     * @param int $digits
+     * @param bool $fullName
+     * @return string
+     */
+    public static function humanReadableBytes($value, $digits = 2, $fullName = false) {
+        $dimensions = [
+            "short"=>["B","KB","MB","GB","TB","PB"],
+            "long"=>["Bytes","Kilobyte","Megabyte","Terabyte","Pentabyte"]
+        ];
+        $factor = 1024;
+        $dim = 0;
+        $buf = $value;
+        while($buf > $factor) {
+            $dim ++;
+            $buf = $buf / $factor;
+        }
+        return sprintf("%02.{$digits}f", $buf)." ".$dimensions[($fullName ? 'long':'short')][$dim];
+    }
+
+    /**
      * Strip non-ascii characters
      * @see http://stackoverflow.com/questions/8781911/remove-non-ascii-characters-from-string-in-php#answer-8781968
 
