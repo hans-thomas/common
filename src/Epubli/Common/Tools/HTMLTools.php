2e5e21c02fa9368d5d55d56711421c92d38a586e
Timor Kodal <timor@kodal.de>
2017-09-11 15:52:16 +0200

    WD-305: introduced new getHtmlContent

--- src/Epubli/Common/Tools/HTMLTools.php
+++ src/Epubli/Common/Tools/HTMLTools.php
@@ -13,4 +13,19 @@ class HTMLTools
     {
         return in_array($name, include('htmlBlockLevelElements.php'));
     }
+
+    public static function stripHtmlTags(
+        $html,
+        $keep =
+        ['br' => '\n', 'p' => '\n', 'h1' => '*$2*', 'h2' => '*$2*', 'h3' => '*$2*', 'h4' => '*$2*', 'h5' => '*$2*', 'span' => '$2', 'a' => '$2', 'div' => '$2'])
+    {
+        $tempFunc = function($matches) use ($keep) {
+            return $matches[2];
+        };
+        $def='[^>]*';
+        $allowedTags = implode(array_keys($keep),"|");
+        $regExp = '@<('.$allowedTags.')>([^<]*)<\/\1>@i';
+        $strippedHtml = strip_tags(preg_replace_callback($regExp,$tempFunc,$html));
+        return $strippedHtml;
+    }
 }
