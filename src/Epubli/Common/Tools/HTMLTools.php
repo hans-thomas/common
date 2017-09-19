823a3a09dfb46a43b43edb096f0d391acad739b1
Timor Kodal <timor@kodal.de>
2017-09-19 08:18:57 +0200

    WD-305: enhanced stripHtmlTags-method (introduced "drop"-argument)

--- src/Epubli/Common/Tools/HTMLTools.php
+++ src/Epubli/Common/Tools/HTMLTools.php
@@ -17,22 +17,32 @@ class HTMLTools
     public static function stripHtmlTags(
         $html,
         $keep =
-        ['br', 'p', 'h1','h2','h3','h4','h5','span','div','i','strong','b'])
+        ['title', 'br', 'p', 'h1','h2','h3','h4','h5','span','div','i','strong','b', 'table', 'td', 'th', 'tr'],
+        $drop =
+        ['head','style']
+    )
     {
+        foreach($drop as $dumpTag) {
+            $html = preg_replace("/<$dumpTag.*$dumpTag>/is","\n",$html);
+        }
+        $html = preg_replace("/[\n\r ]{2,}/i","\n",$html);
+        $html = preg_replace("/[\n|\r]/i",'<br />',$html);
+
         /* @TODO: remove style tags and only keep body content (drop head) */
         $tempFunc = function($matches) use ($keep) {
-            $htmlNode = "<" . $matches[1] . ">" . $matches[2] . "</" . $matches[1] . ">";
+            $htmlNode = "<" . $matches[1] . ">" . strip_tags($matches[2]) . "</" . $matches[1] . ">";
             if (in_array($matches[1],$keep)) {
-                return $htmlNode;
+                return " ".$htmlNode." ";
             } else {
                 return false;
             }
         };
 
-        $def='[^>]*';
         $allowedTags = implode(array_values($keep),"|");
-        $regExp = '@<(.*)[^>]*?>(.*?)<\/\1>@i';
+        $regExp = '@<('.$allowedTags.')[^>]*?>(.*?)<\/\1>@i';
         $strippedHtml = preg_replace_callback($regExp,$tempFunc,$html);
+
+        $strippedHtml = strip_tags($strippedHtml,"<".implode("><",$keep).">");
 
         return $strippedHtml;
     }
