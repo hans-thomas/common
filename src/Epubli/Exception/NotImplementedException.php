ab1ea3b4d40b21c705167208c1d842b0eb2b5d27
Simon Schrape <s.schrape@epubli.com>
2018-02-28 13:58:56 +0100

    Fix Exception class inheritance.

--- src/Epubli/Exception/NotImplementedException.php
+++ src/Epubli/Exception/NotImplementedException.php
@@ -2,16 +2,14 @@
 
 namespace Epubli\Exception;
 
-use Exception;
-
 class NotImplementedException extends Exception
 {
     /**
      * @param string $feature The feature that is not implemented.
      * @param int $code
-     * @param Exception|null $previous
+     * @param \Exception|null $previous
      */
-    public function __construct($feature, $code = 0, Exception $previous = null)
+    public function __construct($feature, $code = 0, \Exception $previous = null)
     {
         parent::__construct("$feature is not yet implemented!", $code, $previous);
     }
--- src/Epubli/Exception/NotSupportedException.php
+++ src/Epubli/Exception/NotSupportedException.php
@@ -2,16 +2,14 @@
 
 namespace Epubli\Exception;
 
-use Exception;
-
 class NotSupportedException extends Exception
 {
     /**
      * @param string $action The action that is not supported.
      * @param int $code
-     * @param Exception|null $previous
+     * @param \Exception|null $previous
      */
-    public function __construct($action, $code = 0, Exception $previous = null)
+    public function __construct($action, $code = 0, \Exception $previous = null)
     {
         parent::__construct("$action is not supported!", $code, $previous);
     }
